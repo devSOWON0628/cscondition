30ce4c447a317443e32fa41fe59f79c1e666c8fc
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-06 17:36:02 +0900

    #3 switch case

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -11,14 +11,45 @@ namespace cscondition
         static void Main(string[] args)
         {
             Console.Write("숫자 입력 : ");
-            int input = int.Parse(Console.ReadLine());
-            if (input % 2 == 0)
+            int input2 = int.Parse(Console.ReadLine());
+            /*    if (input % 2 == 0)
+                {
+                    Console.WriteLine("짝수입니다");
+                }
+               else
+                {
+                    Console.WriteLine("홀수입니다");
+                }*/
+
+            switch (input2)
             {
-                Console.WriteLine("짝수입니다");
-            }
-           else
-            {
-                Console.WriteLine("홀수입니다");
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다");
+                    break;
+
+                default:
+                    Console.WriteLine("엑소플레닛");
+                    break;
             }
             //현재 시간 구하기
 
